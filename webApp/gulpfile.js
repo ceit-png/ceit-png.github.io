d80c8d6d50e5f35deb2aee07605a18b895c335c9
ceit-png <ceit@upng.ac.pg>
2019-09-12 16:16:11 +1000

    moving to gulp model

---
+++ webApp/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
